8603f1d1eb5ba6521c968c0d12df7c1a8fcb435d
Melih Var <varmelih711@gmail.com>
2024-07-15 00:06:06 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,33 +6,24 @@ import (
 	"os"
 )
 
-func clearBoard(board [][]string, freenumbers [][]bool) {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			board[i][j] = "_"
-			freenumbers[i][j] = true
-		}
-	}
-}
-
-func gameboard(board [][]string) {
+func gameboard(board [][]string) { //this is for printing board to screen easily
 	for i := 0; i < len(board); i++ {
 		fmt.Println(board[i])
 	}
 }
 
-func playerchoice(board [][]string, freenumbers [][]bool) {
+func playerchoice(board [][]string, freenumbers [][]bool) { // this is where player make moves
 	for {
 		var row, columm int
 		fmt.Println("Please make a choice:\nrow number (1-3):")
 		fmt.Scanln(&row)
-		row = row - 1
-		fmt.Println("columm number (1-3):")
-		fmt.Scanln(&columm)
-		columm = columm - 1
-		if row <= 3 && columm <= 3 {
+		row = row - 1                       //			>---------------\
+		fmt.Println("columm number (1-3):") //		 			|
+		fmt.Scanln(&columm)                 //					|
+		columm = columm - 1                 //			>----------------\_____> matched user row-column count with
+		if row <= 3 && columm <= 3 {        //								 computer row-column count
 			if freenumbers[row][columm] == true {
-				board[row][columm] = "X"
+				board[row][columm] = "X" // 		replaced "_" with "X"
 				freenumbers[row][columm] = false
 				break
 			} else {
@@ -59,18 +50,18 @@ func ai(board [][]string, freenumbers [][]bool) {
 
 func gamestatus(board [][]string) {
 	if board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" || board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X" || board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X" || board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X" || board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X" || board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X" || board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X" || board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X" {
-		fmt.Println("Player won!")
-		os.Exit(0)
+		fmt.Println("Player won!") //to be honest, i have no better idea than write
+		os.Exit(0)                 //this astronomic "or and" statement
 	}
 	if board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O" || board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O" || board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O" || board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O" || board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O" || board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O" || board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O" || board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O" {
 		fmt.Println("Computer Won!")
-		os.Exit(0)
+		os.Exit(0) //i tried to implement "do you want to restart?" function, but ended up with frustration.
 	}
 }
 
-func engine(board [][]string, freenumbers [][]bool) {
-	for i := 0; i < 4; i++ {
-		fmt.Println("Compuer is making his move...")
+func engine(board [][]string, freenumbers [][]bool) { //this is where is game systemized, first ai then player
+	for i := 0; i < 4; i++ { //and repeat this 4 times. this means 8 moves made in this function. first -or last-
+		fmt.Println("Compuer is making his move...") //is in the main function.
 		ai(board, freenumbers)
 		gameboard(board)
 		gamestatus(board)
@@ -82,35 +73,33 @@ func engine(board [][]string, freenumbers [][]bool) {
 }
 
 func main() {
-	for {
-		fmt.Println("Welcome to XoX game!")
-		fmt.Println("Board initializing...")
-		freenumbers := [][]bool{
-			{true, true, true},
-			{true, true, true},
-			{true, true, true},
-		}
-		_ = freenumbers
-		board := [][]string{
-			{"_", "_", "_"},
-			{"_", "_", "_"},
-			{"_", "_", "_"},
-		}
-		fmt.Println("Game board:")
+	fmt.Println("Welcome to XoX game!")
+	fmt.Println("Board initializing...")
+	freenumbers := [][]bool{
+		{true, true, true},
+		{true, true, true},
+		{true, true, true},
+	}
+	_ = freenumbers
+	board := [][]string{
+		{"_", "_", "_"},
+		{"_", "_", "_"},
+		{"_", "_", "_"},
+	}
+	fmt.Println("Game board:")
+	gameboard(board)
+	if rand.Intn(2) == 1 { //deciding who will be starting first.
+		playerchoice(board, freenumbers) //bright side.
+		fmt.Println("Updated board:")
 		gameboard(board)
-		if rand.Intn(2) == 1 { //deciding who will be starting first.
-			playerchoice(board, freenumbers)
-			fmt.Println("Updated board:")
-			gameboard(board)
-			engine(board, freenumbers)
-			fmt.Println("Game ended in a draw.")
-		} else {
-			engine(board, freenumbers)
-			ai(board, freenumbers)
-			fmt.Println("Updated board:")
-			gameboard(board)
-			gamestatus(board)
-			fmt.Println("Game ended in a draw.")
-		}
+		engine(board, freenumbers)
+		fmt.Println("Game ended in a draw.")
+	} else { //dark side.
+		engine(board, freenumbers)
+		ai(board, freenumbers)
+		fmt.Println("Updated board:")
+		gameboard(board)
+		gamestatus(board)
+		fmt.Println("Game ended in a draw.")
 	}
 }
